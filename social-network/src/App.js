b9f2fff466d1c650574235e60a41aae112644a50
Morgan Lang <morgan@dead-line.org>
2018-05-01 15:02:25 +0000

    CSS is OK, but I was unable to make it modular without errors.

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -6,23 +6,29 @@ class App extends Component {
 
   render(){
 	  return (
-     <div>
-	 <div>
-      <h3>For you</h3>
+     <div id='pagecontainer'>
+	 <h2>For you</h2>
+	 <div id='yourcontainer'>
         {
         your.map(function(yourmapped){
-          return <div class="articlediv"><ul><li>{yourmapped.title}</li></ul></div>;
+          return <div class="yourarticlediv"><ul><li><h3>{yourmapped.title}</h3></li>
+		  <li>{yourmapped.description}</li>
+		  <li><a href={yourmapped.link}>Link</a></li>
+		  <li>Author: {yourmapped.author.name}</li>
+		  <li><img class="authorimage" src={yourmapped.author.image} alt='medium2'/></li>
+		  <li>Posted: {yourmapped.postedDate}</li>
+		  </ul></div>;
         })
         }
         
 
       </div>
-      <div>
-      <h3>In case you missed it</h3>
+	  <h2>In case you missed it</h2>
+      <div id='missedcontainer'>
       {
       missed.map(function(missedmapped){
-        return <div class="articlediv"><ul><li><img class="articleimage" src={missedmapped.image} alt='medium'/></li>
-		<li>{missedmapped.title}</li>
+        return <div class="missedarticlediv"><ul><li><img class="articleimage" src={missedmapped.image} alt='medium'/></li>
+		<li><h3>{missedmapped.title}</h3></li>
         <li>{missedmapped.description}</li>
 	  <li><a href={missedmapped.link}>Link</a></li>
 	  <li>Author: {missedmapped.author.name}</li>
