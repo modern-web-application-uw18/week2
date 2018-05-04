3cc6c61d9cb804e2565745a3ae1b8ff770315ec6
kev-n <k.ng@live.com>
2018-05-03 19:27:42 -0700

    Week 2 Assignment

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -8,26 +8,23 @@ import ArticleHeader from './modules/ArticleHeader';
 
 class App extends Component {
   render() {
-
     console.log(yourArticles);
-
     return (
     <div className="section group">
         <ArticleHeader HeadingText="For you" />
-<article className="your">
-
-        {}
-        {yourArticles.map((article, keyIndex) =>
-          <Card key={keyIndex} article={article}/>
-        )}
-</article>
+          <article className="your">
+            {}
+              {yourArticles.map((article, keyIndex) =>
+               <Card key={keyIndex} article={article}/>
+              )}
+          </article>
         <ArticleHeader HeadingText="In case you missed it" />
-            <article className="missed">
-        {missedArticles.map((article, keyIndex) =>
-          <Card key={keyIndex} article={article}/>
-        )}
-     </article>
-</div>
+          <article className="missed">
+            {missedArticles.map((article, keyIndex) =>
+             <Card key={keyIndex} article={article}/>
+             )}
+          </article>
+     </div>
     );
   }
 }
