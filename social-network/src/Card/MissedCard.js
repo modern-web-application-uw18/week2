b88abb0e5bf60e65152ed714472f8f4100f543d0
bmlebeuf <3bonobos@gmail.com>
2018-06-10 19:48:27 -0700

    Updating files for homework #2

---
+++ social-network/src/Card/MissedCard.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './Card.css';
+import PropTypes from 'prop-types';
+
+class MissedCard extends Component {
+  render() {
+    const {
+      title,
+      image,
+      description,
+      author
+    } = this.props.article;
+    return (
+      <div className="page-box">
+        <div className="flex-container">
+          <div className="Card-box">
+            <img className="Card-img" alt='Article Thumbnail' src={this.props.article.image} />
+            <h2 className="Card-title">{this.props.article.title}</h2>
+            <p className="Card-description">{this.props.article.description}</p>
+            <div className='Card-author'>
+              <img className='authorImage' alt='Author' src={this.props.article.author.image}/>
+              <p className='authorName'>{this.props.article.author.name}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+MissedCard.protoTypes = {
+  article: PropTypes.shape({
+    image: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string
+  })
+};
+
+export default MissedCard;
